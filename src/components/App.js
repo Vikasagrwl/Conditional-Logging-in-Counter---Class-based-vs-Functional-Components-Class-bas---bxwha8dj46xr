6716e34dbbc846be0f3ea74aefdb9413ecbbc4e5
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-06 16:05:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,14 @@ class App extends React.Component{
     this.handleClick = this.handleClick.bind(this)
   }
 
+  shouldComponentUpdate(){
+    if(this.state.count%2!=0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 
   render(){
     console.log(`Rendering with count:-${this.state.count}`)
@@ -21,8 +29,8 @@ class App extends React.Component{
         <button id="incr-btn" onClick={this.handleClick}>Increment</button>
       </div>
     )
-
   }
+  
 }
 
 export default App;
